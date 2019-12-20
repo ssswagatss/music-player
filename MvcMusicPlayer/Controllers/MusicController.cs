67b6d49523656d17341047bb950f42fc0219f01d
Swagat Swain <swagat@mindfiresolutions.com>
2019-12-20 16:57:24 +0530

    Fetches songs

--- MvcMusicPlayer/Controllers/MusicController.cs
+++ MvcMusicPlayer/Controllers/MusicController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcMusicPlayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,7 @@ namespace MvcMusicPlayer.Controllers
         // GET: Music
         public ActionResult Index()
         {
+            var allSongs = Song.GetSongs();
             return View();
         }
 
---
+++ MvcMusicPlayer/Models/Song.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+
+namespace MvcMusicPlayer.Models
+{
+    public class Song
+    {
+        public int SongId { get; set; }
+        public string SongName { get; set; }
+        public string MovieName { get; set; }
+        public int Year { get; set; }
+        public string[] Singers { get; set; }
+        public string Path { get; set; }
+
+        public static IEnumerable<Song> GetSongs()
+        {
+            var songs = new List<Song>();
+            using (StreamReader sr = new StreamReader(HttpContext.Current.Server.MapPath("~/Data/music.json")))
+            {
+                songs.AddRange(JsonConvert.DeserializeObject<List<Song>>(sr.ReadToEnd()));
+            }
+            return songs;
+        }
+    }
+}
